fa77f3c4b6fb3c71e1762a42821acb9749086c6a
Robin TURPIN <robin.turpin@campus.academy>
2020-12-17 12:36:54 +0100

    save

--- python3/niveau-1/niveau-1.py
+++ python3/niveau-1/niveau-1.py
@@ -5,19 +5,25 @@
     17 et 18 décembre 2020
     python 3.7
 """
+width=24
 
-def triangle1(n, width):
+def triangle1(n):
     for size in range(1, n + 1, 2):
         print((size * "*").center(width))
 
-def triangle2(n, width):
+def triangle2(n):
     for size in range(3, n + 1, 4):
         print((size * "*").center(width))
         
-def triangle3(n, width):
+def triangle3(n):
     for size in range(5, n + 1, 6):
         print((size * "*").center(width))
 
-triangle1(8, 24)
-triangle2(16, 24)
-triangle3(24, 24)
+def trunc(n):
+    for size in range(3, n - 4):
+        print((size * "*").center(width))
+
+triangle1(8)
+triangle2(16)
+triangle3(24)
+trunc(10)
